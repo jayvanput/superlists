91f1869e9b07de4d287956e61b102c9d6effc795
jayvanput <jayvanput@gmail.com>
2021-10-29 00:19:05 -0400

    basic home page now returns html

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-import lists
+from lists import views
 from django.urls import path, include
 
 urlpatterns = [
-    path("", lists.views.home_page, name="home")
+    path("", views.home_page)
     # path('admin/', admin.site.urls),
 ]
