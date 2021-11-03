c166f257dd297437f60500ef40709b251192137f
jayvanput <jayvanput@gmail.com>
2021-11-02 23:56:41 -0400

    Added templating to home page for list update

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -35,21 +36,25 @@ class NewVisitorTest(unittest.TestCase):
 
         # When she hits enter, the page updates and now lists "1: Build website"
         inputbox.send_keys(Keys.ENTER)
-
+        time.sleep(1)
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(
-            any(row.text == "1: Write user story" for row in rows),
-            "New to-do item did not appear in table."
-        )
+        self.assertIn("1: Write user story", [row.text for row in rows])
 
         # A text box is still on the screen so she can enter another item. She enters "Launch website"
-        self.fail("Finish the test!")
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Launch website")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again and now shows both of the items.
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn("1: Write user story", [row.text for row in rows])
+        self.assertIn("2: Launch website", [row.text for row in rows])
 
         # She wonders if the website will save the list and notices a unique URL for her.
-
+        self.fail("Finish the test!")
         # She visits the URL and her to-do list is still there.
 
         # She closes out the site.
