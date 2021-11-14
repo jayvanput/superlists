9b3a0d7f38ecaed8c051c216689a24703012313d
jayvanput <jayvanput@gmail.com>
2021-11-14 13:56:35 -0500

    Added function test for 2 users scenario

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -38,12 +38,14 @@ class NewVisitorTest(LiveServerTestCase):
             "Enter a to-do item"
         )
 
-        # She enters "Write user story" into a text box (she is a web developer)
+        # She enters "Write user story" into a text box (she is a web developer) and is taken to a new URL.
         inputbox.send_keys("Write user story")
 
         # When she hits enter, the page updates and now lists "1: Build website"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        alice_list_url = self.browser.current_url
+        self.assertRegex(alice_list_url, "/list/.+")
         self.check_for_row_in_list_table("1: Write user story")
 
         # A text box is still on the screen so she can enter another item. She enters "Launch website"
@@ -55,6 +57,35 @@ class NewVisitorTest(LiveServerTestCase):
         # The page updates again and now shows both of the items.
         self.check_for_row_in_list_table("1: Write user story")
         self.check_for_row_in_list_table("2: Launch website")
+
+        # Now a new user, Bob, comes along to the site.
+
+        # We use a new browser session to make sure that no information
+        # of Alice's is coming through from cookies etc.
+        self.browser.quit()
+        self.browser = webdriver.Firefox(
+            executable_path="C:\Program Files\Mozilla Firefox\geckodriver.exe")
+
+        # Bob visits the home page. There is no sign of Alice's list.
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn("Write user story", page_text)
+        self.assertNotIn("Launch website", page_text)
+
+        # Bob starts a new list by entering a new item.
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Buy milk")
+        inputbox.send_keys(Keys.ENTER)
+
+        # Bob gets his own unique URL
+        bob_list_url = self.browser.current_url
+        self.assertRegex(bob_list_url, "/lists/.+")
+        self.assertNotEqual(bob_list_url, alice_list_url)
+
+        # Again, there is no trace of Alice's list.
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("Write user story", page_text)
+        self.assertIn("Buy milk", page_text)
 
         # She wonders if the website will save the list and notices a unique URL for her.
         self.fail("Finish the test!")
